e1fd94df0dfbc1d060e09d7f6daa93135165ae0b
r <49694912+RicardFabien@users.noreply.github.com>
2024-09-14 18:19:45 +0200

    feat: added first APIs

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Controller\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/messages', function () {
+    return 'Message';
+});
+
+Route::get('/message/{id}',function (string $id) {
+    return 'Message '.$id;
+});
+
